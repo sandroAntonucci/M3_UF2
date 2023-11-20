f34768a362194c2c8260f3669737987b6416b4b4
Sandro <sandro.antonucci.7e7@itb.cat>
2023-11-20 15:02:49 +0200

    update

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApplication
+{
+
+    class NaturalNumbers
+    {
+        public static void Main()
+        {
+
+            const string MsgNum = "Introdueix un nombre natural: ";
+
+            Console.WriteLine(MsgNum);
+            Validate(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine(MsgNum);
+            Validate(Convert.ToInt32(Console.ReadLine()));
+
+        }
+
+        public static void Validate(int num)
+        {
+            Console.WriteLine(num > 0 ? $"El nombre {num} és natural" : $"El nombre {num} no és natural");
+        }
+    }
+}
