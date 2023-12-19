6cc8f5df6d2d1fb0e6f23435f4fbe40873bd9def
sandro <sandro.antonucci.7e7@itb.cat>
2023-12-19 10:58:40 +0100

    update

---
+++ Proves/pruebas/pruebas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyApplication
+{
+
+    class hola
+    {
+
+        static void Main()
+        {
+
+            const string MsgNames = "Introdueix els noms dels personatges (4) separats per comes i un espai: ";
+            const string MsgNamesNotValid = "Aquest format no és vàlid.";
+
+            string[] assignedNames = new string[4];
+
+            string names;
+            bool namesValid = true;
+
+
+            do
+            {
+                Console.WriteLine(MsgNames);
+
+                names = Console.ReadLine();
+                names = names.Trim(',');
+
+
+                if (names.Split(' ').Length != 4)
+                {
+                    Console.Write(MsgNamesNotValid);
+                    namesValid = false;
+                }
+                else
+                {
+                    namesValid = true;
+                    assignedNames = names.Split(' ');
+                }
+
+            } while (!namesValid);
+
+            foreach (string name in assignedNames) Console.WriteLine(name);
+
+        }
+
+    }
+
+
+}
