65cadf94f5eba69026c625cf018ffd35ce3dd150
sandro <sandro.antonucci.7e7@itb.cat>
2024-01-09 13:57:52 +0100

    update

---
+++ Activitats/activitat24/activitat24/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Author: Sandro Antonucci Rejón
+ * M03. Programació UF2
+ * v1. 09/01/2024
+ * Exercici 24. Implementa un programa que demani dos valors per teclat i calculi el seu producte, seguint el mètode rus (multiplicació per duplicació). S’ha d’implementar:
+        mètode iteratiu
+        mètode recursiu
+ *
+ */
+
+
+using System;
+
+namespace MyApplication
+{
+
+    public class Product
+    {
+        public static void Main()
+        {
+
+            const string MsgFirstNum = "Introdueix el primer nombre enter: ";
+            const string MsgSecondNum = "Introdueix el segon nombre enter: ";
+
+            int firstNum, secondNum;
+
+            Console.WriteLine(MsgFirstNum);
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(MsgSecondNum);
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(RecursiveRussianMultiplication(firstNum, secondNum));
+
+
+        }
+
+        public static int IterativeRussianMultiplication(int firstNum, int secondNum)
+        {
+            int result = 0;
+
+            while(firstNum >= 1)
+            {
+                
+                if(firstNum % 2 == 1)
+                {
+                    result += secondNum;
+                }
+
+                firstNum /= 2;
+                secondNum *= 2;
+            }
+
+            return result;
+        }
+
+        public static int RecursiveRussianMultiplication(int firstNum, int secondNum)
+        {
+
+            if(firstNum == 1)
+            {
+                return secondNum;
+            }
+            else if (firstNum % 2 == 1)
+            {
+                return secondNum + RecursiveRussianMultiplication(firstNum / 2, secondNum * 2);
+            }
+            else
+            {
+                return RecursiveRussianMultiplication(firstNum / 2, secondNum * 2);
+            }
+            
+        }
+    }
+
+}
+
---
+++ Activitats/activitat25/activitat25/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Author: Sandro Antonucci Rejón
+ * M03. Programació UF2
+ * v1. dd/mm/yyyy
+ * Exercici X. Enunciat de l’exercici que es resol
+ *
+ */
+
+
+using System;
+
+namespace MyApplication
+{
+    public class Binary
+    {
+        public static void Main()
+        {
+
+            const string MsgNum = "Introdueix un nombre enter: ";
+            const string MsgEnd = "\nPrem una tecla per continuar.";
+
+            int num;
+
+            Console.Write(MsgNum);
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(RecursiveBinary(num, ""));
+
+
+            Console.WriteLine(MsgEnd);
+            Console.ReadKey();
+        }
+
+        public static string RecursiveBinary(int num, string currentNum)
+        {
+            
+            if (num == 1) return "1" + currentNum;
+
+            else
+            {
+                if (num % 2 == 1)
+                {
+                    currentNum = "1" + currentNum;
+                } 
+                else
+                {
+                    currentNum = "0" + currentNum;
+                }
+                Console.WriteLine(currentNum);
+                return RecursiveBinary(num / 2, currentNum);
+            }
+
+        }
+    }
+}
+
